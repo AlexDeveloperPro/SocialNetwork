74c98c6967d7d73979bae8379928618c4317b238
AlexDeveloperPro <alex-prohorov-dev@ya.ru>
2025-03-13 19:44:23 +0300

    test commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,11 @@ import '../css/bootstrap.min.css'
 // import.meta.glob([
 //     '../images/**',
 // ]);
-
+//
 
 // import Alpine from 'alpinejs';
 
 // window.Alpine = Alpine;
 
 // Alpine.start();
+
